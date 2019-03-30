4ebe73e0f8b437a7d19d626850d6c4254fb87756
Blodjer <j95.szwaczka@gmail.com>
2019-03-30 20:39:42 +0100

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,22 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+	filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
